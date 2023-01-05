a2bafb05e20b7ce2d4a74b8c1b0c6629b5554801
nikodem <nikodemmarek11@gmail.com>
2023-01-05 18:10:13 +0100

    feat: main visuals

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express')
+const fs = require('fs')
 const path = require('path')
+
+const hbs = require('express-handlebars')
+const formidable = require('formidable')
 
 const PORT = 3000
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.engine(
+    'hbs',
+    hbs({
+        defaultLayout: 'main.hbs',
+        extname: '.hbs',
+        partialsDir: path.join('views', 'partials'),
+    })
+)
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.end('ok')
+    res.redirect('/filemanager')
+})
+
+app.get('/filemanager', (req, res) => {
+    res.render('filemanager.hbs')
+})
+
+app.post('/upload', (req, res) => {
+    const form = formidable({
+        multiples: true,
+        uploadDir: path.join(__dirname, 'data'),
+        keepExtensions: true,
+    })
+
+    form.parse(req, (err, fields, files) => {})
 })
 
 app.listen(PORT, () => {
