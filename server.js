a1a01ff868bb5dd3f7cd8a4fdbd9443f41ccd1fa
nikodem <nikodemmarek11@gmail.com>
2023-01-06 12:18:10 +0100

    fix: cancel closes the dialog, separate endpoints for new folder and file

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ const formidable = require('formidable')
 const PORT = 3000
 const app = express()
 
+const parseDirs = (raw) =>
+    raw
+        .split(/\/|\\/g)
+        .map((part) => part.trim())
+        .filter((item) => item !== '')
+const pth = (...dirs) => path.join(__dirname, 'data', ...dirs)
+
+const createFolder = (dirs) => {
+    const path = pth(...dirs)
+
+    if (!fs.existsSync(path)) fs.mkdirSync(path, {recursive: true})
+
+    return path
+}
+const createFile = (dirs, name, content = '') => {
+    if (!fs.existsSync(pth(...dirs))) createFolder(dirs)
+
+    const path = pth(...dirs, name)
+
+    fs.writeFileSync(path, content)
+
+    return path
+}
+
 app.set('views', path.join(__dirname, 'views'))
 app.engine(
     'hbs',
@@ -39,17 +63,16 @@ app.post('/upload', (req, res) => {
         console.log(files)
     })
 })
-app.post('/create', (req, res) => {
-    const fullPath = req.body.path
-        .split(/\/|\\/g)
-        .map((part) => part.trim())
-        .filter((item) => item !== '')
-    const [file, absolutePath] = [
-        fullPath.pop(),
-        path.join(__dirname, ...fullPath),
-    ]
+app.post('/create/folder', (req, res) => {
+    const fullPath = parseDirs(req.body.path)
 
-    console.log(absolutePath, file)
+    createFolder(fullPath)
+})
+app.post('/create/file', (req, res) => {
+    const fullPath = parseDirs(req.body.path)
+    const [file, ...dirs] = [fullPath.pop(), ...fullPath]
+
+    createFile(dirs, file)
 })
 
 app.listen(PORT, () => {
