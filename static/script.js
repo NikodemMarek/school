5ae64555e913f90b14d5694cbefc07e33f843f91
nikodem <nikodemmarek11@gmail.com>
2023-03-03 19:21:58 +0100

    feat: convert to react

--- static/script.js
+++ static/script.js
@@ -1,47 +1,90 @@
-;(async () => {
-    const room = 'room1'
-    const username = prompt('username')
+const Message = ({user, message, time, type}) => {
+    const isSelf = user === username
 
-    const msgContainer = document.querySelector('#messages')
+    return (
+        <div
+            className={`message${
+                isSelf ? (type === 'msg' ? ' right' : ' left') : ' left'
+            }`}
+        >
+            <div className='msg-user'>{user}</div>
+            <div className={`msg-message ${isSelf ? 'self-' : ''}${type}-msg`}>
+                {message}
+            </div>
+            <div className='msg-time'>{time.toLocaleTimeString()}</div>
+        </div>
+    )
+}
 
-    const createMsg = (user, message, time, type) => {
-        const isSelf = user === username
+const Chat = ({messages}) => (
+    <div id='messages'>
+        {messages.map((msg, key) => (
+            <Message {...msg} key={key} />
+        ))}
+    </div>
+)
 
-        const msg = document.createElement('div')
-        msg.classList.add('message')
-        msg.classList.add(isSelf ? (type === 'msg' ? 'right' : 'left') : 'left')
-        msg.innerHTML = `
-            <div class="msg__user">${user}</div>
-            <div class="msg__message ${
-                isSelf ? 'self-' : ''
-            }${type}-msg">${message}</div>
-            <div class="msg__time">${time.toLocaleTimeString()}</div>
-        `
+const MessageBox = ({value, send}) => {
+    const [message, setMessage] = React.useState(value || '')
 
-        return msg
+    const sendTyped = () => {
+        // if (message === '') return // uncomment to disable sending empty messages
+
+        send(message)
+        setMessage('')
     }
 
-    const socket = io()
-    socket.emit('join', {room, username})
+    return (
+        <div id='msg-form'>
+            <input
+                id='new-msg'
+                type='text'
+                value={message}
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter') sendTyped()
+                }}
+                onChange={(e) => setMessage(e.target.value)}
+                autoComplete='on'
+                autoFocus='on'
+            />
+            <button id='send-msg' onClick={sendTyped}>
+                Send
+            </button>
+        </div>
+    )
+}
 
-    const messageInput = document.querySelector('#new-msg')
-    const sendTypedMsg = () => {
-        const msg = messageInput.value
-        // if (!msg) return // uncomment to disable sending empty messages
+const Whole = ({socket}) => {
+    const [messages, setMessages] = React.useState([])
 
+    const sendMsg = (msg) => {
         socket.emit('msg', {room, user: username, message: msg})
-        messageInput.value = ''
-        messageInput.focus()
     }
 
-    document.onkeyup = (e) => {
-        if (e.key === 'Enter') sendTypedMsg()
-    }
-    document.querySelector('#send-msg').onclick = () => sendTypedMsg()
+    React.useEffect(() => {
+        socket.on('msg', ({user, message, type}) => {
+            setMessages((messages) => [
+                ...messages,
+                {user, message, time: new Date(), type},
+            ])
 
-    socket.on('msg', ({user, message, type}) => {
-        msgContainer.appendChild(createMsg(user, message, new Date(), type))
+            scrollTo(0, document.body.scrollHeight)
+        })
+    }, [])
 
-        scrollTo(0, document.body.scrollHeight)
-    })
-})()
+    return (
+        <div>
+            <Chat messages={messages} />
+
+            <MessageBox send={sendMsg} />
+        </div>
+    )
+}
+
+const room = 'room1'
+const username = prompt('username')
+
+const socket = io()
+socket.emit('join', {room, username})
+
+ReactDOM.render(<Whole socket={socket} />, document.body)
