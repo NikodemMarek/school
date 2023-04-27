ad5faa628e927940ab6bd66e7a35d317107fcdd8
nikodem <nikodemmarek11@gmail.com>
2023-04-27 12:42:59 +0200

    style: some rearrangement

--- app/images/router.js
+++ app/images/router.js
@@ -1,23 +1,6 @@
 const {albums, Album, Photo} = require('./model')
 
 const router = (entry, server) => {
-    server.post(`${entry}`, async ({body: {albumName}, files}) => {
-        const photos = files.map(
-            (file) => new Photo(file.originalName, file.path)
-        )
-
-        const album = Album.get(albumName)
-        for (const photo of photos) {
-            await album.addPhoto(photo)
-        }
-
-        return {
-            body: JSON.stringify(albums),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
-    })
     server.get(`${entry}`, async () => ({
         body: JSON.stringify(albums),
         headers: {
@@ -35,6 +18,25 @@ const router = (entry, server) => {
             },
         }
     })
+
+    server.post(`${entry}`, async ({body: {albumName}, files}) => {
+        const photos = files.map(
+            (file) => new Photo(file.originalName, file.path)
+        )
+
+        const album = Album.get(albumName)
+        for (const photo of photos) {
+            await album.addPhoto(photo)
+        }
+
+        return {
+            body: JSON.stringify(albums),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+    })
+
     server.delete(`${entry}/:id`, async ({params: {id: rawId}}) => {
         const id = parseInt(rawId)
         albums.map(album => album.deletePhoto(id))
