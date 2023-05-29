def0f1a4c1f365693d3cb935e4e2a48601b965af
nikodem <nikodemmarek11@gmail.com>
2023-05-29 23:10:56 +0200

    feat: better ai

--- src/app/opponentai.ts
+++ src/app/opponentai.ts
@@ -1,7 +1,7 @@
 import { Mark } from "./types";
 
 export class Opponent {
-  private nextAttack: { x: number, y: number } | null = null;
+  private attackMap: { x: number, y: number }[] | null = null;
 
   constructor(private size: { x: number, y: number }) {}
 
@@ -10,26 +10,92 @@ export class Opponent {
 
     const getRandomMove = () => moves[Math.floor(Math.random() * moves.length)];
 
+    if (!this.attackMap) {
+      const bestPoints = this.getBestPoints(board)
+      this.attackMap = bestPoints[Math.floor(Math.random() * bestPoints.length)]
+    }
+    if (this.attackMap.some(({ x, y }) => x === lastMove.x && y === lastMove.y)) {
+      const bestPoints = this.getBestPoints(board)
+      this.attackMap = bestPoints[Math.floor(Math.random() * bestPoints.length)]
+    }
+
     const saveMoves = this.shouldIBeAlamed(board, lastMove);
     if (saveMoves.length > 0)
       return saveMoves[0]
 
-    if (this.nextAttack == lastMove)
-      this.nextAttack = null
 
-    if (!this.nextAttack) {
-      console.log('0')
-      const thisAttack = getRandomMove()
-      this.nextAttack = this.getNextAttack(board, thisAttack)
-      console.log('0', thisAttack)
-      return thisAttack
-    } else {
-      console.log('1')
-      const thisAttack = this.nextAttack
-      this.nextAttack = this.getNextAttack(board, thisAttack)
-      console.log('1', thisAttack)
-      return thisAttack
+    return this.attackMap.find(({ x, y }) => board[y][x] === Mark.None) || getRandomMove()
+  }
+
+  private getBestPoints = (board: Mark[][]) => {
+    const freeCombinations = [
+      [
+        { x: -2, y: 0 },
+        { x: -1, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+      ],
+      [
+        { x: 0, y: -2 },
+        { x: 0, y: -1 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+      ],
+      [
+        { x: -2, y: -2 },
+        { x: -1, y: -1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 },
+      ],
+      [
+        { x: -2, y: 2 },
+        { x: -1, y: 1 },
+        { x: 1, y: -1 },
+        { x: 2, y: -2 },
+      ],
+    ]
+
+    const pointsToCheck: { x: number, y: number }[] = []
+    for (let y = 0; y < board.length; y++) {
+      const row = board[y];
+      for (let x = 0; x < row.length; x++) {
+        if (board[y][x] === Mark.None || board[y][x] === Mark.X)
+          pointsToCheck.push({ x, y })
+      }
     }
+
+    const bests = pointsToCheck.map(({ x, y }) => {
+       const combinations = freeCombinations.filter(([a, b, c, d]) =>
+          (board[y + a.y]?.[x + a.x] === Mark.X || board[y + a.y]?.[x + a.x] === Mark.None)
+          && (board[y + b.y]?.[x + b.x] === Mark.X || board[y + b.y]?.[x + b.x] === Mark.None)
+          && (board[y + c.y]?.[x + c.x] === Mark.X || board[y + c.y]?.[x + c.x] === Mark.None)
+          && (board[y + d.y]?.[x + d.x] === Mark.X || board[y + d.y]?.[x + d.x] === Mark.None)
+      )
+
+      return combinations.map(([a, b, c, d]) => {
+        return [
+          {
+            x: x + a.x,
+            y: y + a.y,
+          },
+          {
+            x: x + b.x,
+            y: y + b.y,
+          },
+          { x, y },
+          {
+            x: x + c.x,
+            y: y + c.y,
+          },
+          {
+            x: x + d.x,
+            y: y + d.y,
+          },
+        ]
+      })
+    })
+
+    return bests.flat()
   }
 
   private getMoves = (board: Mark[][]): { x: number, y: number }[] => {
@@ -43,93 +109,6 @@ export class Opponent {
     }
 
     return moves;
-  }
-
-  private tilesWithMarkAound = (board: Mark[][], around: { x: number, y: number }, mark: Mark): { x: number, y: number }[] => {
-    const { x, y } = around;
-
-    const possibilities = [
-      { x: 1, y: 1 },
-      { x: 0, y: 1 },
-      { x: -1, y: 1 },
-      { x: -1, y: 0 },
-      { x: -1, y: -1 },
-      { x: 0, y: -1 },
-      { x: 1, y: -1 },
-      { x: 1, y: 0 },
-    ];
-
-    const tilesAround: { x: number, y: number }[] = [];
-    for (const possibility of possibilities) {
-      const newX = x + possibility.x;
-      const newY = y + possibility.y;
-
-      if (newX >= 0 && newX < this.size.x && newY >= 0 && newY < this.size.y)
-        if (board[newY][newX] === mark)
-          tilesAround.push({ x: newX, y: newY });
-    }
-
-    return tilesAround;
-  }
-
-  private getMovesAround = (board: Mark[][], origin: { x: number, y: number }): { x: number, y: number }[] =>
-    this.tilesWithMarkAound(board, origin, Mark.None);
-
-  private getMyMarksAround = (board: Mark[][], origin: { x: number, y: number }): { x: number, y: number }[] =>
-    this.tilesWithMarkAound(board, origin, Mark.X);
-
-  private getOppositeMoves = (moves: { x: number, y: number }[], origin: { x: number, y: number }): { x: number, y: number }[] =>
-    moves.map(({ x, y }) => ({
-      x: 2 * origin.x - x,
-      y: 2 * origin.y - y,
-    }))
-  private getOppositeFreeMoves = (board: Mark[][], origin: { x: number, y: number }): { x: number, y: number }[] => {
-    // console.log('or', origin)
-
-    const rel = this.getMovesAround(board, origin)
-    // console.log('rel', rel)
-
-    const opp = this.getOppositeMoves(rel, origin)
-    // console.log('opp', opp)
-    const filt = opp.filter(({ x, y }) => board[y]?.[x] === Mark.None)
-    // console.log('filt', filt)
-    return filt
-  }
-
-  private getNextAttack = (board: Mark[][], nextAttack: { x: number, y: number }): { x: number, y: number } | null => {
-    const myTilesAround = this.getMyMarksAround(board, nextAttack)
-
-    if (myTilesAround.length <= 0) {
-      // console.log('a')
-      const movesAround = this.getMovesAround(board, nextAttack)
-
-      if (movesAround.length <= 0) {
-        // console.log('b')
-        return null
-      } else {
-        // console.log('c')
-        return movesAround[0]
-      }
-    } else {
-      // console.log('d')
-      const freeOpposite = this.getOppositeFreeMoves(board, nextAttack)
-
-      if (freeOpposite.length <= 0) {
-        // console.log('e')
-        const movesAround = this.getMovesAround(board, nextAttack)
-
-        if (movesAround.length <= 0) {
-          // console.log('f')
-          return null
-        } else {
-          // console.log('g')
-          return movesAround[0]
-        }
-      } else {
-        // console.log('h')
-        return freeOpposite[0]
-      }
-    }
   }
 
   private shouldIBeAlamed = (board: Mark[][], lastMove: { x: number, y: number }): { x: number, y: number }[] => {
