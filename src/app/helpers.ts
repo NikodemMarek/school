43d4b15aab3fd64544fc44b27e1e3becb8b91522
nikodem <nikodemmarek11@gmail.com>
2023-06-07 13:30:59 +0200

    feat: handle special cases

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -63,8 +63,9 @@ const MagazineDB = {
                                 year,
                                 publications.filter(publication =>
                                     publication?.number?.startsWith(year)
-                                    || year === 'nr specjalne' && publication?.number?.includes('nn')
-                                    // TODO: add other special cases here
+                                    || name === 'Bajtek' && year === 'nr specjalne' && publication?.number?.match(/^[0-9]{4}$|^nn$/)
+                                    || name === 'Komputer' && year === 'nr specjalny' && publication?.number === '1988/7' && publication?.pages === 48
+                                    || name === 'IKS' && year === 'nr specjalne' && publication?.name === 'IKS Zeszyty Programów Komputerowych'
                                 )
                             )),
                     )
@@ -72,6 +73,7 @@ const MagazineDB = {
                     acc.push(czasopismo);
                     return acc;
                 }, [] as Magazine[])
+                console.log(this.magazines);
 
                 this.loaded = true;
             })
@@ -87,73 +89,6 @@ const MagazineDB = {
 }
 
 export default MagazineDB;
-
-// class MagazinesDB {
-//     public czasopisma: Magazine[] = [];
-//
-//     public get = () => this.czasopisma;
-//     public getNames = () => this.czasopisma
-//         .map(czasopismo => czasopismo.name);
-//     public getMagazine = (name: string) => this.czasopisma
-//         .find(czasopismo => czasopismo.name === name);
-//
-// 	constructor(private http: HttpClient) {
-//         this.load();
-//     }
-//
-//     private load = () =>
-//         this.http.get(DATA_URL, { responseType: 'text' })
-//             .subscribe(data => {
-//                 const rawMagazines = this.convertToJSON(data).czasopisma
-//
-//                 this.czasopisma = Object.entries(rawMagazines).reduce((acc, [name, value]) => {
-//                     if (name === 'zmienne' || name === 'lata')
-//                         return acc;
-//
-//
-//                     const publications = Object.values(value as any[])
-//                         .map(publication => new Publication(
-//                             publication.nazwa,
-//                             publication.numer,
-//                             parseInt(publication.stron),
-//
-//                             PUBLICATION_ASSETS_URL + '/' + name + '/' + publication.miniaturka,
-//                             publication.format,
-//                             publication.plik,
-//
-//                             publication.podeslal,
-//                             publication.przetworzenie,
-//                             publication.skan,
-//                             publication.wydawca,
-//                         ));
-//                     const czasopismo = new Magazine(
-//                         name,
-//                         MAGAZINE_ASSETS_URL + rawMagazines.zmienne[name].src,
-//                         rawMagazines.lata[name].split(',')
-//                             .map((year: string) => new Year(
-//                                 year,
-//                                 publications.filter(publication =>
-//                                     publication?.number?.startsWith(year)
-//                                     || year === 'nr specjalne' && publication?.number?.includes('nn')
-//                                     // TODO: add other special cases here
-//                                 )
-//                             )),
-//                     )
-//
-//                     acc.push(czasopismo);
-//                     return acc;
-//                 }, [] as Magazine[])
-//             });
-//
-//     private convertToJSON = (xml: string) => {
-//         let res: any = null
-//
-//         const parser = new xml2js.Parser({explicitArray: false});
-//         parser.parseString(xml, (err, result) => res = result);
-//
-//         return res;
-//     }
-// }
 
 class Magazine {
     constructor(
